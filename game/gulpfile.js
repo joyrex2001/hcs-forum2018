af37a1cecd5ac026063d88d7630cd6309013de18
Vincent van Dam <vincentd@erg.verweg.com>
2018-07-26 20:03:11 +0200

    Add dev watch task

--- game/gulpfile.js
+++ game/gulpfile.js
@@ -6,15 +6,17 @@ const uglify = require("gulp-uglify");
 
 const dest = "./htdocs";
 
+var js = [
+  "./frontend/js/lib/phaser.min.js",
+  "./frontend/js/lib/box2d-plugin-full.js",
+  "./frontend/js/websock.js",
+  "./frontend/js/player.js",
+  "./frontend/js/game.js",
+  "./frontend/js/util.js"
+]
+
 function build() {
-  gulp.src([
-      "./frontend/js/lib/phaser.min.js",
-      "./frontend/js/lib/box2d-plugin-full.js",
-      "./frontend/js/websock.js",
-      "./frontend/js/player.js",
-      "./frontend/js/game.js",
-      "./frontend/js/util.js"
-    ])
+  gulp.src(js)
     .pipe(concat("game.js"))
     .pipe(uglify({ compress: { hoist_funs: true } }))
     .pipe(gulp.dest(dest));
@@ -29,6 +31,13 @@ function build() {
     .pipe(gulp.dest(dest + "/assets"));
 }
 
+function dev() {
+  build();
+  gulp.watch(js,['build']);
+}
+
 gulp.task('default', build);
+gulp.task('build', build);
+gulp.task('dev', dev);
 
 module.exports = build;
